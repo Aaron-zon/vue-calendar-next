2771ac23d8e6fd1445adfb8d49cf0c9df1aff9ea
Aaron-zon <1640485761@qq.com>
2024-03-26 22:24:17 +0800

    set path alias

--- packages/components/button/index.ts
+++ packages/components/button/index.ts
@@ -1,3 +1,7 @@
 import button from './src/index.vue'
-import { withInstall } from '../../utils/install'
+// import { withInstall } from '../../utils/index'
+// import { withInstall } from '@vue-calendar-next/packagesutils'
+// import { withInstall } from '@pk/utils/install'
+import { withInstall } from '@pk/utils/install'
+console.log(withInstall)
 export const ElButton = withInstall(button)
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@pk': fileURLToPath(new URL('./package', import.meta.url))
     }
   }
 })
